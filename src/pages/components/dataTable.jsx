5b1e36bf7302529e4cb6c9f1eba8f212d5a13b82
lingao570 <lingao570@lu.com>
2020-05-07 21:55:30 +0800

    add table

--- src/pages/components/dataTable.jsx
+++ src/pages/components/dataTable.jsx
@@ -1,65 +1,41 @@
-import React from 'react';
-import { Table, Button } from 'antd';
+import React, { useState, useEffect }from 'react';
+import { Table } from 'antd';
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-  },
-];
+const DataTable = (props) => {
+  const [columns, setColumns] = useState([]);
+  const [scrollX, setScrollX] = useState(800);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+  });
+  useEffect(() => {
+    let columnData = props.tableData && props.tableData[0] && Object.keys(props.tableData[0]).map((key, index) => {
+      return {
+        title: key,
+        width: (Math.floor(key.length / 5) || 1) * 100,
+        dataIndex: key,
+        key,
+        fixed: index === 0 ? 'left' : false,
+      }
+    })
+    const tableData = props.tableData.map((item, index) => {
+      item.key = index;
+      return item;
+    })
+    setScrollX(columnData.reduce((current, next) => {
+      return current + next.width + 5;
+    }, 0))
+    setColumns(tableData);
+    console.log("Did Mount")
+  }, [])
 
-
-
-class DataTable extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  state = {
-    selectedRowKeys: [], // Check here to configure the default column
-    loading: false,
-  };
-
-  start = () => {
-    this.setState({ loading: true });
-    // ajax request after empty completing
-    setTimeout(() => {
-      this.setState({
-        selectedRowKeys: [],
-        loading: false,
-      });
-    }, 1000);
-  };
-
-  onSelectChange = selectedRowKeys => {
-    this.setState({ selectedRowKeys });
-  };
-
-  render() {
-    const { loading, selectedRowKeys } = this.state;
-    const rowSelection = {
-      selectedRowKeys,
-      onChange: this.onSelectChange,
-    };
-    const hasSelected = selectedRowKeys.length > 0;
-    return (
-      <div>
-        <div style={{ marginBottom: 16 }}>
-          <span style={{ marginLeft: 8 }}>
-            {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
-          </span>
-        </div>
-        <Table rowSelection={rowSelection} columns={columns} dataSource={this.props.myData} />
+  return (
+    <div>
+      <div style={{ marginBottom: 16 }}>
       </div>
-    );
-  }
+      <Table columns={columns} dataSource={props.tableData} scroll={{ x: scrollX, y: 800 }} />
+    </div>
+  );
 }
 
 export default DataTable;
